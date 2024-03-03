d39d8e29db4da00818097456363b782aea5b7923
Ansh Poonia <pooniaansh11@gmail.com>
2024-03-04 00:24:19 +0530

    first

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM almalinux:latest
+
+RUN dnf install -y gcc g++ make python3 python3-pip espeak-ng
+
+WORKDIR /root
+
+COPY . /root/
+
+RUN pip3 install llvmlite --ignore-installed
+RUN pip3 install torch torchaudio
+RUN pip3 install -r requirements.txt
+
+EXPOSE 5000
+
+CMD ["python3", "app.py"]
---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, send_file, request, jsonify, redirect, url_for
+import hashlib
+from TTS.api import TTS
+import os
+
+app = Flask(__name__)
+tts = TTS(model_name="tts_models/en/jenny/jenny", progress_bar=False)
+
+
+def hashit(input_string):
+    input_bytes = input_string.encode('utf-8')
+    sha256_hash = hashlib.sha256()
+    sha256_hash.update(input_bytes)
+    hashed_string = sha256_hash.hexdigest()
+    return hashed_string
+
+
+def voiceit(text, loc):
+    tts.tts_to_file(text=text, file_path=f"files/{loc}.wav")
+
+
+@app.route('/')
+def index():
+    try:
+        phone_number = request.args.get("p")
+        text = request.args.get("text")
+        # model = request.args.get("v")
+        loc = hashit(str(phone_number) + text)
+        if not os.path.isfile(os.path.join("files", f"{loc}.wav")):
+            voiceit(text, loc)
+    except:
+        return jsonify({"message": "Use the following get parameters",
+                        "p": "Phone Number",
+                        "text": "Text that need to be converted into speech",
+                        "v": "Selected model"})
+    else:
+        return redirect(url_for("file", h=loc))
+
+
+@app.route('/file')
+def file():
+    h = request.args.get("h")
+    return send_file(f"files/{h}.wav")
+
+
+if __name__ == "__main__":
+    app.run(debug=False, host='0.0.0.0')
