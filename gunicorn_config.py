ded64b5f2a161c6735e66c6b230a396c51f5be09
Ansh Poonia <pooniaansh11@gmail.com>
2024-04-16 00:34:09 +0530

    cert

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -2,5 +2,5 @@ bind = '0.0.0.0:443'
 workers = 1
 timeout = 600
 limit_request_line = 0
-certfile = '/root/ssl/certificate.crt'
-keyfile = '/root/ssl/private.key'
+certfile = '/root/.acme.sh/tts.apimerger.com_ecc/fullchain.cer'
+keyfile = '/root/.acme.sh/tts.apimerger.com_ecc/ca.cer'
