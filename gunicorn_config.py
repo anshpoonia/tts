4892d25bd1065ec6667359826dd5bad84ad600f9
Ansh Poonia <pooniaansh11@gmail.com>
2024-04-16 01:35:01 +0530

    cert

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -2,5 +2,5 @@ bind = '0.0.0.0:5000'
 workers = 1
 timeout = 600
 limit_request_line = 0
-# certfile = '/root/ssl/cert.pem'
-# keyfile = '/root/ssl/key.pem'
+certfile = '/root/ssl/cert.pem'
+keyfile = '/root/ssl/key.pem'
